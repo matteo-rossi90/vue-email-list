58f45f7781f9a35675e96eaeea4a9976fa7655d0
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-06-27 16:11:53 +0200

    feat: add mail list in DOM from API

--- js/main.js
+++ js/main.js
@@ -2,3 +2,32 @@
 generare 10 indirizzi email e stamparli in pagina all’interno di una lista. */
 
 const { createApp } = Vue;
+
+createApp({
+    
+    //elenco dati
+    data(){
+
+        return{
+
+            //creare un array vuoto
+            mailList: [
+            ]
+        }
+    },
+    mounted(){
+
+        //inserire 10 mail nell'array vuoto
+        for (let i = 0; i < 10; i++) {
+
+            //richiamo della API che genera le mail
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((risposta) => {
+                    const result = risposta.data.response;
+                    console.log(result);
+                    this.mailList.push(result);
+                })
+        }
+    }
+
+}).mount("#app")
