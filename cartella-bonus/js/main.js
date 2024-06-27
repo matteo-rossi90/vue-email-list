ac3ecd474fb7a17518576cf85d2eac4d4999d248
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-06-27 16:59:30 +0200

    feat: add bonus - insert emails in the list only when generated from API

---
+++ cartella-bonus/js/main.js
@@ -0,0 +1,38 @@
+/*Attraverso l’apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail 
+generare 10 indirizzi email e stamparli in pagina all’interno di una lista. */
+
+/*Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati. */
+
+const { createApp } = Vue;
+
+createApp({
+    
+    //elenco dati
+    data(){
+
+        return{
+
+            //creare un array vuoto
+            mailList: []
+        }
+    },
+    mounted(){
+
+        //inserire 10 mail nell'array vuoto
+        for (let i = 0; i < 10; i++) {
+
+            //richiamo della API che genera le mail
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((risposta) => {
+                    const result = risposta.data.response;
+                    console.log(result);
+                    this.mailList.push(result);
+                })
+                .catch(error => {
+                    console.error('Errore nella richiesta:', error);
+                });
+        }
+    }
+
+}).mount("#app")
